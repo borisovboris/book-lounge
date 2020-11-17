0352652d67237c0800e6699e31fb7a9dfc8cd2dd
borisovboris <boris.s.borisov99@gmail.com>
2020-11-17 17:24:51 +0300

    added landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
