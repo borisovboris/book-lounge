cede48b279df33fe09b68deb85c00a5e296ad76b
borisovboris <boris.s.borisov99@gmail.com>
2020-11-18 22:28:43 +0300

    scroll service with throttle

--- src/app/core/services/scroll.service.ts
+++ src/app/core/services/scroll.service.ts
@@ -1,7 +1,7 @@
 import { ViewportScroller } from '@angular/common';
 import { ElementRef, Injectable } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, throttleTime } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +9,14 @@ import { debounceTime } from 'rxjs/operators';
 export class ScrollService  {
 
   private prevScrollPos: number = 0;
-  private debounceInterval = 18;
+  private debounceInterval = 175;
 
   constructor(private viewportScroller: ViewportScroller) { 
 
   }
 
   manageNavbar(navbar: ElementRef) {
-    fromEvent(window, 'scroll').pipe(debounceTime(this.debounceInterval)).subscribe(
+    fromEvent(window, 'scroll').pipe(throttleTime(this.debounceInterval)).subscribe(
       () => { 
         console.log('scroll');
         const currentScrollPos = this.viewportScroller.getScrollPosition()[1];
