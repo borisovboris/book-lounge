e93b70c8fb668ead333d5b97422d096a6899d73d
borisovboris <boris.s.borisov99@gmail.com>
2020-11-18 17:37:43 +0300

    further improved navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  private prevScrollPos: number = 0;
+
+  @ViewChild(NavComponent, {read: ElementRef}) navbar: ElementRef;
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
+    const currentScrollPos = window.pageYOffset;
+    const el = this.navbar.nativeElement;
+
+    if(currentScrollPos > this.prevScrollPos) {
+      el.style.top = "-80px";
+      console.log('down');
+    } else {
+      el.style.top = "0px";
+      console.log('up');
+    }
+    
+    this.prevScrollPos = currentScrollPos;
+  } 
+
+  ngAfterViewInit() {
+    // console.log(this.navbar.nativeElement.style.display = "none");
+    // console.log(this.navbar.nativeElement);
+  }
   
 }
