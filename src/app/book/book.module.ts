fb445c1beb7fb5078776c34297523830164076f8
borisovboris <boris.s.borisov99@gmail.com>
2020-11-25 18:54:21 +0300

    fixed layout

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
-import { BookComponent } from './book/book.component';
 import { BookListComponent } from './book-list/book-list.component';
 
 
 @NgModule({
-  declarations: [BookComponent, BookListComponent],
+  declarations: [BookListComponent],
   imports: [
     CommonModule,
     BookRoutingModule
