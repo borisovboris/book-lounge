be1765b73fa8fe007615d96deb3093a4b1cffab6
borisovboris <boris.s.borisov99@gmail.com>
2020-12-08 23:45:04 +0300

    added reviews mock data

---
+++ src/app/core/mock-data/reviews.ts
@@ -0,0 +1,10 @@
+import { IReview } from '../models/review';
+
+export const reviews: IReview[] = [
+    {id: 1, userId: 1, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'},
+    {id: 2, userId: 2, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'},
+    {id: 3, userId: 3, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'},
+    {id: 4, userId: 4, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'},
+    {id: 5, userId: 5, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'},
+    {id: 6, userId: 6, content: 'lorem, lorem, lorem, lorem, lorem, lorem, lorem, lorem'}
+];
---
+++ src/app/core/models/review.ts
@@ -0,0 +1,5 @@
+export interface IReview {
+    id: number;
+    userId: number;
+    content: string;
+}
