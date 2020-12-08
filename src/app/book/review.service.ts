b8715df21c22da8cf9faec11b3b3214609a8b7e4
borisovboris <boris.s.borisov99@gmail.com>
2020-12-08 23:58:20 +0300

    added review service

---
+++ src/app/book/review.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { reviews } from '../core/mock-data/reviews';
+import { IReview } from '../core/models/review';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  public books: BehaviorSubject<IReview[]> = new BehaviorSubject<IReview[]>(reviews);
+  public books$ = this.books.asObservable();
+
+  constructor() { }
+
+  getAllReviews(): Observable<IReview[] | IReview | Observable<IReview[]>> {
+    return of(this.books$).pipe(delay(1000));
+  }
+
+  addReview(review: IReview[]): void {
+   
+  }
+
+}
