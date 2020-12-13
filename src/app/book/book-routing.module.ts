0690de8fcef9da7c07774f5377aae0db0db03c3b
borisovboris <boris.s.borisov99@gmail.com>
2020-12-13 16:57:30 +0300

    added auth guard

--- src/app/book/book-routing.module.ts
+++ src/app/book/book-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { BookDetailsResolverService } from './book-details-resolver.service';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListComponent } from './book-list/book-list.component';
 
 const bookRoutes: Routes = [
   { path: '', children: [
-    { path: 'book-details/:id', component: BookDetailsComponent, resolve: { book: BookDetailsResolverService} },
+    { 
+      path: 'book-details/:id', 
+      component: BookDetailsComponent, 
+      resolve: { book: BookDetailsResolverService},
+      canActivate: [AuthGuard]
+    },
     { path: '', component: BookListComponent, pathMatch: 'full' }
   ]}
 ];
