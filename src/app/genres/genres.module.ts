556531b671e0c814848c91a82f2871aee8eae76e
borisovboris <boris.s.borisov99@gmail.com>
2020-12-15 22:10:38 +0300

    responsive genres-all

--- src/app/genres/genres.module.ts
+++ src/app/genres/genres.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GenresRoutingModule } from './genres-routing.module';
-import { GenresAllComponent } from './genres-all/genres-all.component';
 import { GenresAllListComponent } from './genres-all-list/genres-all-list.component';
 
 
 @NgModule({
-  declarations: [GenresAllComponent, GenresAllListComponent],
+  declarations: [GenresAllListComponent],
   imports: [
     CommonModule,
     GenresRoutingModule
